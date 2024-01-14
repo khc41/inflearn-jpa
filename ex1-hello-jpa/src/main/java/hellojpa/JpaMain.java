6907148c6ec258df1311946733f1ed188f62d773
heechul <khc4190@naver.com>
2024-01-14 14:58:12 +0900

    proxy 예외 발생 (준영속상태)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,31 +20,27 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsarname("member1");
             em.persist(member1);
-            Member member2 = new Member();
-            member2.setUsarname("member2");
-            em.persist(member2);
-            
+
             em.flush();
             em.clear();
 
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember.getClass() = " + refMember.getClass());
 
-            logic(m1, m2);
+//            em.detach(refMember);
+            em.clear();
+
+            refMember.getUsarname();
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
 
         emf.close();
-    }
-
-    private static void logic(Member m1, Member m2) {
-        System.out.println("m1 == m2: " + (m1 instanceof Member));
-        System.out.println("m1 == m2: " + (m2 instanceof Member));
     }
 
 }
