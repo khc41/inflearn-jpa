0014361505c06fc17add8c931fca1987f35b2d59
heechul <khc4190@naver.com>
2024-01-15 20:39:20 +0900

    jpql에서 n+1문제 일으킴 - 쿼리 두번 나감

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -29,13 +30,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
+//            Member m = em.find(Member.class, member1.getId());
 
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
-            System.out.println("============");
-            m.getTeam().getName(); //초기화
-            System.out.println("============");
 
             tx.commit();
         } catch (Exception e) {
