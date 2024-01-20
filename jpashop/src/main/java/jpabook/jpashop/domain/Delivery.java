aeea808a63ddd1f6a821d87eafdb8f71c8bb4a5e
heechul <khc4190@naver.com>
2024-01-20 19:17:53 +0900

    값타입 적용 실습

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,17 +1,21 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.*;
 
 @Entity
+@Getter
+@Setter
 public class Delivery extends BaseEntity{
     @Id
     @GeneratedValue
     private Long id;
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     private DeliveryStatus staus;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
