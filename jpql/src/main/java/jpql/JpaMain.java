a597f658c1e69ed42813f8708bff52814977f058
heechul <khc4190@naver.com>
2024-01-20 20:10:00 +0900

    jpql 기본문법

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,7 +19,23 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result.getUsername());
+
+
+//            List<Member> resultList = query.getResultList();
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+//
+//            Member result = query.getSingleResult();
+//            System.out.println("result = " + result);
+
 
             tx.commit();
         } catch (Exception e) {
