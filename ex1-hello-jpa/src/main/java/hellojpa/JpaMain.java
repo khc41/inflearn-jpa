5ae5de801746e89f02bd232b45380501d4269859
heechul <khc4190@naver.com>
2024-01-20 19:25:19 +0900

    jpql 예시

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,39 +19,14 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsarname("member1");
-            member.setHomeAddress(new Address("homeCity","street","10000"));
+            List<Member> result = em.createQuery(
+                    "select m from Member m where m.usarname like '%kim%'",
+                    Member.class
+            ).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-//            member.getAddressHistory().add(new Address("old1","street","10000"));
-//            member.getAddressHistory().add(new Address("old2","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old1","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street","10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("========== START =============");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1","street","10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1","street","10000"));
-            findMember.getAddressHistory().remove(new AddressEntity("old1","street","10000"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1","street","10000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
