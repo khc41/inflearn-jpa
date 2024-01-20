fbe0a43e38f24581c2e5450abc0b21d2ba52b10f
heechul <khc4190@naver.com>
2024-01-20 18:36:36 +0900

    값타입 비교

--- ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Getter
 @Embeddable
@@ -15,4 +16,17 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
