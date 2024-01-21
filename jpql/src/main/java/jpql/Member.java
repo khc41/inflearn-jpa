a14891abf257e2c0baf3817c1dac92df22a87669
heechul <khc4190@naver.com>
2024-01-21 20:35:21 +0900

    jpql - 페이징 처리

--- jpql/src/main/java/jpql/Member.java
+++ jpql/src/main/java/jpql/Member.java
@@ -2,12 +2,14 @@ package jpql;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@ToString
 public class Member {
     @Id
     @GeneratedValue
